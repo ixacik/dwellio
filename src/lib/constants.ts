8981a2959cb94edf29917dbf0918c36715b66919
Patrik <patriklevak@gmail.com>
2024-04-05 18:43:28 +0200

    drizzle, pg setup, ui updates

--- src/lib/constants.ts
+++
@@ -1,6 +0,0 @@
-export const NAV_LINKS = [
-  { href: "/", label: "Home" },
-  { href: "/search", label: "Search" },
-  { href: "/explore", label: "Explore" },
-  { href: "/create", label: "Create" },
-];
